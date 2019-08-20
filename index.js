efaaa43c8b5017e599f9ac5e687856f6e642fa75
sunnyjagtap1 <53648900+sunnyjagtap1@users.noreply.github.com>
2019-08-20 15:42:17 +0530

    Dice

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageScource1 = "dice" + randomNumber1 + ".png";
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImageScource1);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var randomImageScource2 = "dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageScource2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🏆Player 1 Wins!";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "🏆Player 2 Wins!";
+} else {
+  document.querySelector("h1").innerHTML = "It's a Draw!";
+}
